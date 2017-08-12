7d422997a281ddefee5b0689488a1daa8741945c
Darnell W <darnellwillis96@gmail.com>
2017-08-12 15:36:43 -0700

    Final

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,79 +1,78 @@
 $(document).ready(function() {
 
-var winCounter = 0;
-var lossesCounter = 0;
+	crystals = ['assets/images/red.png','assets/images/blue.png','assets/images/yellow.png','assets/images/green.png'];
 
-/*
-var redGem = Math.floor((Math.random() * 12) + 1);
-var blueGem = Math.floor((Math.random() * 12) + 1);
-var yellowGem = Math.floor((Math.random() * 12) + 1);
-var greenGem = Math.floor((Math.random() * 12) + 1);
-*/
+	var counter = 0;
+	var wins = 0;
+	var losses = 0;
+	$('#win').text(wins);
+	$('#loss').text(losses);
+	
+	newCrystals();
+	newGame();
 
-var totalScore = 0;
+	function newCrystals () {
+		var numbers = []
+			while(numbers.length < 4){
+			  var randomnumber = Math.ceil(Math.random()*12)
+			  var found = false;
+			  for (var i=0; i< numbers.length; i++){
+				if (numbers[i] == randomnumber){
+					found = true; break
+				}
+			  }
+			  if(!found)numbers[numbers.length]=randomnumber;
+			}
+		console.log(numbers);		
 
-var targetNumber = Math.floor((Math.random() * 102) + 19);
+		for (i = 0; i < numbers.length; i++) {
+			var imageCrystal = $('<img>');
+			imageCrystal.attr('data-num', numbers[i]);
+			imageCrystal.attr('src', crystals[i]);
+			imageCrystal.attr('alt', 'crystals');
+			imageCrystal.addClass('crystalImage')
+			$('#crystals').append(imageCrystal);
+		}
+	}
+
+	function newGame() {
+
+		counter = 0;
+		$('#yourScore').text(counter);
+
+		function randomIntFromInterval(min,max){
+		   	return Math.floor(Math.random()*(max-min+1)+min);
+			}
+
+		var numberToGuess = randomIntFromInterval(19,120);
+
+		$('.value').text(numberToGuess);
 
 
-function startGame() {
+		$('.crystalImage').on('click', function(){
+		    counter = counter + parseInt($(this).data('num'));
+		   
+		    $('#yourScore').text(counter);
 
-// displays the targer number in a green background
-$("#number-to-guess").html(targetNumber);  
-
-
-var cyristalClicks = {
-    redGem: Math.floor((Math.random() * 12) + 1),
-    blueGem: Math.floor((Math.random() * 12) + 1),
-    greenGem: Math.floor((Math.random() * 12) + 1),
-    yellowGem: Math.floor((Math.random() * 12) + 1)
-};
-
-$(".cyristal-click").on("click", function () {
-
-    $("#total-score").html(cyristalClicks);
+		    if (counter == numberToGuess){
+		      $('#status').text('You won!!!!');
+		      wins ++;
+		      $('#win').text(wins);
+		      console.log(wins)
+		      $('#crystals').empty();
+		      newCrystals();
+		      newGame();
+		        
+		    } else if ( counter > numberToGuess){
+		        $('#status').text('You lost!')
+		        losses ++;
+		        $('#loss').text(losses);
+		        console.log(losses)
+		        $('#crystals').empty();
+		        newCrystals();
+		        newGame();
+		    }
+		});
+	}
 
 });
-
-$("#blue-gem).on("click", function() {
-
-    $("#total-score").html(blueGem);
-
- $("#green-gem").on("click", function() {
-
-    $("#total-score").html(greenGem);    
-
-$("#red-gem").on("click", function() {
-
-    $("#total-score").html(redGem); 
-    
-$("#yellow-gem").on("click", function() {
-
-    $("#total-score").html(yellowGem);   
-
-     if (targetNumber === totalScore) {
-        $("#wins").html(winCounter);
-        alert("You win!");
-        winCounter++;
-    } else if (totalScore >= targetNumber) {
-        $("#losses").html(lossesCounter);
-        alert("You have lose!, Please try again");
-        lossesCounter++;
-    }
-
-}); // yellow function
-
-}); // red function
-
-}); // green function
-
-}); //blue function
-
-
-}
-} //startGame function
-
-
-startGame();
-
-
-}); //.ready function
